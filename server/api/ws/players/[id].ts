c94dc11d67e780f173b0e77ec0cc420bc6c9aa5a
Gabriel Novack <45369167+gnovack41@users.noreply.github.com>
2025-07-03 23:15:52 -0400

    Add Zod Validation and Player Name List (#3)

--- server/api/ws/players/[id].ts
+++ server/api/ws/players/[id].ts
@@ -1,68 +1,94 @@
-import { PLAYER_JOINED_MESSAGE, PLAYER_LEFT_MESSAGE, SESSION_ENDED } from '~/utils';
+import { Map } from "~/utils/services/maps";
+import type { MapInfo } from "~/utils";
+import { MessageData, MessageType, PeerInfo } from "~/utils";
 
 function getTopicFromUrl(url: string): string {
     const urlParts = url.split('/');
     return urlParts[urlParts.length - 1];
 }
 
-const mapToPeerId: Record<string, string> = {};
+export const mapToPeerId: Record<string, MapInfo> = {};
 
 export default defineWebSocketHandler({
     open(peer) {
         const topic = getTopicFromUrl(peer.request.url);
 
         peer.subscribe(topic);
-
-        peer.publish(
-            topic,
-            JSON.stringify({
-                type: PLAYER_JOINED_MESSAGE,
-                player_count: peer.peers.size,
-            }),
-        );
     },
     async message(peer, message) {
         const topic = getTopicFromUrl(peer.request.url);
 
-        const messageText = message.text();
+        const messageData: MessageData = MessageData.parse(message.json());
 
-        if (mapToPeerId[topic]) {
-            if (mapToPeerId[topic] !== peer.id) {
-                throw createError({
-                    statusCode: 403,
-                    statusMessage: 'Only the creator of the map may update it.',
-                });
+        if (messageData.type === MessageType.MAP_UPDATE) {
+            Map.parse(messageData.data);
+
+            if (mapToPeerId[topic]?.ownerPeerId) {
+                if (mapToPeerId[topic].ownerPeerId !== peer.id) {
+                    throw createError({
+                        statusCode: 403,
+                        statusMessage: 'Only the creator of the map may update it.',
+                    });
+                }
+            } else {
+                mapToPeerId[topic] = {
+                    ownerPeerId: peer.id,
+                    playerPeers: {},
+                }
             }
-        } else {
-            mapToPeerId[topic] = peer.id;
-        }
+        } else if (messageData.type === MessageType.PLAYER_JOINED) {
+            const peerInfo = PeerInfo.parse(messageData.data);
 
-        peer.publish(topic, messageText);
-    },
-    close(peer) {
-        const topic = getTopicFromUrl(peer.request.url);
+            if (!mapToPeerId[topic]) {
+                mapToPeerId[topic] = {
+                    ownerPeerId: null,
+                    playerPeers: {
+                        [peer.id]: peerInfo,
+                    }
+                }
+            }
 
-        peer.unsubscribe(topic);
-
-        if (mapToPeerId[topic] === peer.id) {
-            delete mapToPeerId[topic];
+            mapToPeerId[topic].playerPeers[peer.id] = peerInfo;
 
             peer.publish(
                 topic,
                 JSON.stringify({
-                    type: SESSION_ENDED,
-                    player_count: peer.peers.size,
+                    type: MessageType.PLAYER_JOINED,
+                    data: Object.entries(mapToPeerId[topic].playerPeers).map(([_, info]) => info),
                 }),
             );
 
             return;
         }
 
+        peer.publish(topic, JSON.stringify(messageData));
+    },
+    close(peer) {
+        const topic = getTopicFromUrl(peer.request.url);
+
+        peer.unsubscribe(topic);
+
+        if (mapToPeerId[topic]?.ownerPeerId === peer.id) {
+            peer.publish(
+                topic,
+                JSON.stringify({
+                    type: MessageType.SESSION_ENDED,
+                    data: Object.entries(mapToPeerId[topic].playerPeers).map(([_, info]) => info),
+                }),
+            );
+
+            delete mapToPeerId[topic];
+
+            return;
+        }
+
+        delete mapToPeerId[topic]?.playerPeers[peer.id];
+
         peer.publish(
             topic,
             JSON.stringify({
-                type: PLAYER_LEFT_MESSAGE,
-                player_count: peer.peers.size,
+                type: MessageType.PLAYER_LEFT,
+                data: Object.entries(mapToPeerId[topic].playerPeers).map(([_, info]) => info),
             }),
         );
     },
--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,40 @@
+import { z } from "zod";
+import { Map } from "~/utils/services/maps";
+
 export function generateRandomString(length: number): string {
     return Math.random().toString(36).substring(2, length + 2);
 }
 
-export const PLAYER_JOINED_MESSAGE = 'player_joined';
-export const PLAYER_LEFT_MESSAGE = 'player_left';
+export enum MessageType {
+    PLAYER_JOINED = 'player_joined',
+    PLAYER_LEFT = 'player_left',
+    SESSION_ENDED = 'session_ended',
+    MAP_UPDATE = 'map_update',
+}
 
-export const SESSION_ENDED = 'session_ended';
+export const PeerInfo = z.object({
+    name: z.string(),
+})
+
+export type PeerInfo = z.infer<typeof PeerInfo>;
+
+export const MapInfo = z.object({
+    ownerPeerId: z.string().nullable(),
+    playerPeers: z.record(z.string(), PeerInfo),
+});
+
+export type MapInfo = z.infer<typeof MapInfo>;
+
+export const MessageData = z.object({
+    type: z.nativeEnum(MessageType),
+    data: z.union([Map, PeerInfo]),
+})
+
+export type MessageData = z.infer<typeof MessageData>;
+
+export const PublishedData = z.object({
+    type: z.nativeEnum(MessageType),
+    data: z.union([Map, z.array(PeerInfo)]),
+});
+
+export type PublishedData = z.infer<typeof PublishedData>;
